834012d3200cd589bf18f6c6f2cd5fa60215cd51
jscamariel <jessicaml866@gmail.com>
2020-01-30 16:46:27 +0700

    wow

--- nore/app/src/main/java/com/nore/kalkulator_gampang/Bayar.java
+++ nore/app/src/main/java/com/nore/kalkulator_gampang/Bayar.java
@@ -305,7 +305,7 @@ public class Bayar extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(uang1+uang2+uang3+uang4+uang5+uang6+uang7+uang8+uang9+uang10 < pricetotal){
-                    kembalian.setText("Maaf, uangnya kurang..");
+                    kembalian.setText("Uang kurang");
                 }else{
                     kembalian.setText("Rp."+String.valueOf((uang1+uang2+uang3+uang4+uang5+uang6+uang7+uang8+uang9+uang10)-pricetotal));
                 }
